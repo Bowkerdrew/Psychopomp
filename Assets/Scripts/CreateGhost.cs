ba10a86874af4a9574d3aed67dfc73a5982a038c
Drew Bowker <44102655+Bowkerdrew@users.noreply.github.com>
2023-01-30 14:19:47 -0700

    Add object to plane

--- Assets/Scripts/CreateGhost.cs
+++ Assets/Scripts/CreateGhost.cs
@@ -21,10 +21,21 @@ static List<ARRaycastHit> hits = new List<ARRaycastHit>();
     private void Awake(){
         _arRaycastManager = GetComponent<ARRaycastManager>();
     }
-
+    bool TryGetTouchPosition(out Vector2 touchPosition){
+        
+        if(Input.touchCount > 0){
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
 
     void Update()
     {
+        if(!TryGetTouchPosition(out Vector2 touchPosition)){
+            return;
+        }
         if(_arRaycastManager.Raycast(touchPosition, hits, trackableTypes: TrackableType.PlaneWithinPolygon))
         {
 
