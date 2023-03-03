fa373b662f29047e0efb7a4b28b825c4b1a4205c
Esi-Mena <mena7940@bears.unco.edu>
2023-03-02 18:13:02 -0700

    Throwing
    Made code for throwing the object

--- Assets/Scripts/CreateGhost.cs
+++ Assets/Scripts/CreateGhost.cs
@@ -14,6 +14,24 @@ public class CreateGhost : MonoBehaviour
     public int deathCount;
     private ARRaycastManager _arRaycastManager;
     private Vector2 touchPosition;
+    public float minThrowSwipeDistance;
+    private Vector2 touchStartPosition = Vector2.zero;
+
+    [Header("References")]
+    public Transform cam;
+    public Transform attackPoint;
+    public GameObject holywater;
+
+    [Header("Settings")]
+    public int totalThrows;
+    public float throwCooldown;
+
+    [Header("Throwing")]
+    public KeyCode throwKey= KeyCode.Mouse0;
+    public float throwForce;
+    public float throwUpwardForce;
+
+    bool readyToThrow;
 
 
     private Vector3 cameraPosition;
@@ -38,6 +56,7 @@ public class CreateGhost : MonoBehaviour
     {
         
         ghost.AddComponent<BoxCollider>();
+        readyToThrow=true;
         
     }
     void Update()
@@ -68,34 +87,60 @@ public class CreateGhost : MonoBehaviour
 
 
             lookCamera();
-            CallWhenScreenTouched();
+           
             checkKill();
             deathCheck();
         }
         if (SpawnedGhost != null && SpawnedGhost.GetComponent<Collider>() == null)
-{
-    SpawnedGhost.AddComponent<BoxCollider>();
-}
-
-
-    }
-    void CallWhenScreenTouched()
-{
-    if (Input.touchCount > 0)
-    {
-        var touch = Input.GetTouch(0);
-        var ray = Camera.main.ScreenPointToRay(touch.position);
-        RaycastHit hitInfo;
-        if (Physics.Raycast(ray, out hitInfo))
         {
-            if (hitInfo.collider.gameObject == SpawnedGhost) 
-            {
-                Destroy(SpawnedGhost);
-                SpawnedGhost = null;
-            }
+            SpawnedGhost.AddComponent<BoxCollider>();
         }
+
+       // if(Input.GetKeyDown(throwKey)&& readyToThrow && totalThrows > 0)
+       // {
+           // Throw();
+       // }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+              touchStartPosition = Input.GetTouch(0).position;
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            Vector2 touchEndPosition = Input.GetTouch(0).position;
+            if (Vector2.Distance(touchStartPosition, touchEndPosition) >= minThrowSwipeDistance && readyToThrow && totalThrows > 0)
+            {
+            Throw();
+            }
+       }
+
     }
-}
+    void Throw()
+    {
+         Debug.Log("Throw() function called.");
+   
+
+        readyToThrow = false;
+        
+
+        GameObject projectile = Instantiate(holywater,attackPoint.position, cam.rotation);
+       
+        Debug.Log("Projectile instantiated.");
+
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        Vector3 forceToAdd = cam.transform.forward * throwForce+ transform.up*throwUpwardForce;
+
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        totalThrows--;
+
+        Invoke(nameof(ResetThrow), throwCooldown);
+    } 
+
+    void ResetThrow()
+    {
+        readyToThrow=true;
+    }
 
 
 
@@ -140,5 +185,7 @@ public class CreateGhost : MonoBehaviour
             SceneManager.LoadScene("Death");
         }
     }
+
+
 
 }
